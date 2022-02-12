a1c9716519cd0f4ca9d43484ec795601b6528e3d
Tayyab-it <tayyabaman80@gmail.com>
2022-02-13 00:36:16 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace File_Handling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.view_source = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.view_source)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(57, 40);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(100, 20);
+            this.Name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(223, 36);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // view_source
+            // 
+            this.view_source.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.view_source.Location = new System.Drawing.Point(195, 129);
+            this.view_source.Name = "view_source";
+            this.view_source.Size = new System.Drawing.Size(196, 183);
+            this.view_source.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.view_source);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Name);
+           
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.view_source)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.DataGridView view_source;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Handling
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable();
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.table.Columns.Add("NAME");
+            view_source.DataSource = this.table;
+            read_file();
+            view_source.Refresh();
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            DataRow record = this.table.NewRow();
+            record["NAME"] = Name.Text;
+            this.table.Rows.Add(record);
+            write_file();
+            view_source.Refresh();
+        }
+
+        //Write File
+        public void write_file()
+        {
+            string path = Directory.GetCurrentDirectory() + @"\DataFile.txt";
+            if (File.Exists(path)) { File.Delete(path); }
+            FileStream fs = File.Open(path, FileMode.Create);
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                foreach (DataRow row in this.table.Rows)
+                    sw.WriteLine(row["NAME"]);
+            }
+        }
+        //Read File
+        public void read_file()
+        {
+
+            string path = Directory.GetCurrentDirectory() + @"\DataFile.txt";
+
+            if (File.Exists(path))
+            {
+
+                FileStream fs = File.Open(path, FileMode.Open);
+                using (StreamReader sr = new StreamReader(fs))
+                {
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        DataRow record = this.table.NewRow();
+                        record["NAME"] = sr.ReadLine();
+                        this.table.Rows.Add(record);
+                    }
+
+                }
+            }
+        }
+
+        
+    }
+}
